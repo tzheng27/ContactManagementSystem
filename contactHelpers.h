0ddeca4d259d54b823931743f5d10cf009d9544c
Tian Zheng <zhengtian07@gmail.com>
2019-02-05 11:56:03 -0500

    Update contactHelpers.h

--- contactHelpers.h
+++ contactHelpers.h
@@ -1,30 +1,6 @@
-/* ------------------------------------------------------------------
-Name:				Tian Zheng
-Student number:		155 394 174
-Email:				tzheng26@myseneca.ca
-Section:			GG
-Date:				Aug 4, 2018
----------------------------------------------------------------------
-Assignment: 2
-Milestone:  4
----------------------------------------------------------------------
-Description: This header file modularizes general helper functions to
-help reduce redundant coding for common repetative tasks.
----------------------------------------------------------------------*/
-
-
-// Hint:  This header file now has functions with parameters referring to 
-//        the struct Contact type so be sure to include the contacts.h header:
 #include "contacts.h"
 
-//------------------------------------------------------
 // Function Prototypes
-//------------------------------------------------------
-
-// +-------------------------------------------------+
-// | NOTE:  Copy/Paste your Assignment-2 Milestone-3 |
-// |        function prototypes here...              |
-// +-------------------------------------------------+
 
 // Clear the standard input buffer
 // clearKeyboard:

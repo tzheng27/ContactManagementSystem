218bc4eb4b781315213abb1bc161e28cba4f6978
Tian Zheng <zhengtian07@gmail.com>
2019-02-05 11:55:12 -0500

    Update contacts.h

--- contacts.h
+++ contacts.h
@@ -1,34 +1,7 @@
-// ----------------------------------------------------------------------------
-// DO NOT remove the below 2 lines.
-// The below two lines is needed to ensure only one copy
-// of this header file is included when compiling the project.
-// (it is referred to as "safeguarding")
-// You will learn about this in C++ (OOP244/BTP200)
-// For now, simply leave this code in!
 #ifndef CONTACTS_H_
 #define CONTACTS_H_
-// ----------------------------------------------------------------------------
-
-/* ------------------------------------------------------------------
-Name:				Tian Zheng
-Student number:		155 394 174
-Email:				tzheng26@myseneca.ca
-Section:			GG
-Date:				Aug 4, 2018
-----------------------------------------------
-Assignment: 2
-Milestone:  4
----------------------------------------------- */
 
 
-//------------------------------------------------------
-// Structure Types
-//------------------------------------------------------
-
-// +-------------------------------------------------+
-// | NOTE:  Copy/Paste your Assignment-2 Milestone-3 |
-// |        structures below:                        |
-// +-------------------------------------------------+
 
 // Structure type Name declaration
 struct Name {
@@ -60,14 +33,9 @@ struct Contact {
 	struct Numbers numbers;
 };
 
-//------------------------------------------------------
-// Function Prototypes
-//------------------------------------------------------
 
-// +-------------------------------------------------+
-// | NOTE:  Copy/Paste your Assignment-2 Milestone-3 |
-// |        function prototypes below...             |
-// +-------------------------------------------------+
+
+// Function Prototypes
 
 // getName:
 void getName(struct Name*);
@@ -83,6 +51,4 @@ void getContact(struct Contact *);
 
 
 
-// ----------------------------------------------------------------------------
-// DO NOT remove the below line (closing of the "safeguard" condition)
 #endif // !CONTACTS_H_
